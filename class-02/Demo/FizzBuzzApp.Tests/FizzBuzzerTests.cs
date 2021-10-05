795b053a07a284f0cb3672366a434fa541cfa774
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-05 11:30:42 -0500

    Buzz for 5

--- class-02/Demo/FizzBuzzApp.Tests/FizzBuzzerTests.cs
+++ class-02/Demo/FizzBuzzApp.Tests/FizzBuzzerTests.cs
@@ -34,5 +34,47 @@ namespace FizzBuzzApp.Tests
             // Assert
             Assert.Equal("2", result);
         }
+
+        [Fact]
+        public void FizzBuzz_returns_Fizz_given_3()
+        {
+            // Arrange
+            FizzBuzzer fb = new FizzBuzzer();
+
+            // Act
+            string result = fb.FizzBuzz(3);
+
+            // Assert
+            Assert.Equal("Fizz", result);
+        }
+
+        [Fact]
+        public void FizzBuzz_returns_Fizz_given_6()
+        {
+            // Arrange
+            FizzBuzzer fb = new FizzBuzzer();
+
+            // Act
+            string result = fb.FizzBuzz(6);
+
+            // Assert
+            Assert.Equal("Fizz", result);
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(20)]
+        public void FizzBuzz_returns_Buzz_for_multiples_of_five(int number)
+        {
+            // Arrange
+            FizzBuzzer fb = new FizzBuzzer();
+
+            // Act
+            string result = fb.FizzBuzz(number);
+
+            // Assert
+            Assert.Equal("Buzz", result);
+        }
     }
 }
--- class-02/Demo/FizzBuzzApp/FizzBuzzer.cs
+++ class-02/Demo/FizzBuzzApp/FizzBuzzer.cs
@@ -10,6 +10,16 @@ namespace FizzBuzzApp
     {
         public string FizzBuzz(int number)
         {
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+
             return number.ToString();
         }
     }
