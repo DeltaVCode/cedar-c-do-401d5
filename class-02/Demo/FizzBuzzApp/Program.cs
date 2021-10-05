ab459d20c76297c3f738ba956011346760f15a07
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-05 10:23:26 -0500

    Add empty console/test projects

---
+++ class-02/Demo/FizzBuzzApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzzApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
