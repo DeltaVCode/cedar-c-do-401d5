d09be9249ce6a5b360f0828411ae03927bb86006
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-06 11:15:18 -0500

    Empty demo

---
+++ class-03/Demo/FileDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
