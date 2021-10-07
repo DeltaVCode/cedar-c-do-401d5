b6030553495e1def3517c79e0e6f565f19ec269d
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-07 11:56:23 -0500

    Add Book with ctor with title, and prop/method

---
+++ class-04/Demo/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Book
+    {
+        // Field = class instance variable
+        private readonly string title;
+
+        // Constructor!
+        public Book(string title)
+        {
+            // TODO: test this
+            if (title == null) throw new ArgumentNullException(nameof(title));
+
+            this.title = title;
+        }
+
+        // Properties
+        public string Title
+        {
+            get { return this.title; }
+        }
+
+
+        // Methods
+
+        // Example that's equivalent to the Title property
+        public string GetTitle()
+        {
+            return this.title;
+        }
+
+
+        // author
+        // chapters
+        // publish year
+        // page count
+        // isbn
+    }
+}
---
+++ class-04/Demo/BookTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Demo
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Book_constructor_requires_title()
+        {
+            // Arrange
+
+
+            // Act
+            Book book = new Book("Lord of the Fries");
+
+            // Assert
+            Assert.Equal("Lord of the Fries", book.Title);
+
+            Assert.Equal("Lord of the Fries", book.GetTitle());
+        }
+    }
+}
