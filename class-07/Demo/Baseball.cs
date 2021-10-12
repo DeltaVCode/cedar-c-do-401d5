e2c02df8bbda0f1ceb7c1c2f1332b7df856849a7
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-12 09:04:21 -0500

    Copy forward Demo 6

---
+++ class-07/Demo/Baseball.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Demo
+{
+    class Baseball : Sport
+    {
+        public Baseball(string name) : base(name)
+        {
+        }
+
+        public override bool IsIndoors => false; // shortcut for get { return false; }
+
+        public override string[] GetEquipment()
+        {
+            return new string[]
+            {
+                "Baseball",
+                "Bats",
+                "Gloves",
+                "Big League Chew",
+            };
+        }
+
+        public override void PrintRules()
+        {
+            Console.WriteLine("Scratch yourself");
+        }
+    }
+}
