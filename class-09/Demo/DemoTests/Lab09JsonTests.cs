0f872429c0296f9d25b6e47e3d8d86d0728e3acb
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-14 12:06:38 -0500

    Read from data.json

---
+++ class-09/Demo/DemoTests/Lab09JsonTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoTests
+{
+    public class Lab09JsonTests
+    {
+        [Fact]
+        public void Can_Read_File()
+        {
+            string json = File.ReadAllText("data.son");
+            Assert.NotEmpty(json);
+        }
+    }
+}
