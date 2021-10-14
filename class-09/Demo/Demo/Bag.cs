cde5cd551d078cdae8c3edb287953ff27df2baf9
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-14 09:03:07 -0500

    Copy forward Demo 08

---
+++ class-09/Demo/Demo/Bag.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Bag<T> : IEnumerable<T>
+    {
+        int count = 0;
+        T[] stuff = new T[2];
+
+        public int Count => count;
+
+        public void Add(T value)
+        {
+            if (count >= stuff.Length)
+            {
+                Array.Resize(ref stuff, stuff.Length * 2);
+            }
+
+            stuff[count] = value;
+            count++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            // Use iterator to return empty (for now)
+            // yield break;
+
+            for (int i = 0; i < count; i++)
+            {
+                yield return stuff[i];
+            }
+        }
+
+        // Explicit implementation of the interface member
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            // Call the real one above
+            return GetEnumerator();
+        }
+    }
+}
---
+++ class-09/Demo/DemoTests/IEnumerableGenericTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DemoTests
+{
+    public class IEnumerableGenericTests
+    {
+        [Fact]
+        public void ArrayIsIEnumerable()
+        {
+            int[] numbers = new[] { 1, 2, 3 };
+
+            ForeachAndAssertNotZero(numbers);
+
+            EnumerateAndAssertNotZero(numbers);
+        }
+
+        private void EnumerateAndAssertNotZero(IEnumerable<int> sequence)
+        {
+            IEnumerator<int> enumerator = sequence.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                int number = enumerator.Current;
+                Assert.NotEqual(0, number);
+            }
+        }
+
+        private void ForeachAndAssertNotZero(IEnumerable<int> seq)
+        {
+            // Cross our fingers that number is actually an int
+            foreach (int number in seq)
+            {
+                Assert.NotEqual(0, number);
+            }
+        }
+
+        [Fact]
+        public void ListIsIEnumerable()
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(1);
+            numbers.Add(2);
+            // numbers.Add("3"); // compiler *can* prevent this
+            numbers.Add(4);
+
+            EnumerateAndAssertNotZero(numbers);
+
+            ForeachAndAssertNotZero(numbers);
+        }
+    }
+}
