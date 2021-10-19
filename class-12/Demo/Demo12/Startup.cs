b3c66f5d70a34f0f06dab82352bbf118f83947b0
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-19 11:58:58 -0500

    Configure EF to use DefaultConnection

--- class-12/Demo/Demo12/Startup.cs
+++ class-12/Demo/Demo12/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Demo12.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +15,26 @@ namespace Demo12
 {
     public class Startup
     {
+        // 1. Add property to hold Config
+        public IConfiguration Configuration { get; }
+
+        // 2. Magic to get an IConfiguration from somewhere
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SchoolDbContext>(options => {
+                // Our process.env.DATABASE_URL from js days
+                string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+                // Use that connection string with SQL Server
+                options.UseSqlServer(connectionString);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
