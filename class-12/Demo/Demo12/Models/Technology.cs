d8a6d6af9ea368b5a1db70ec84cb9a076a683fcc
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-19 12:11:53 -0500

    Require Technology.Name

--- class-12/Demo/Demo12/Models/Technology.cs
+++ class-12/Demo/Demo12/Models/Technology.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace Demo12.Models
     public class Technology
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
     }
 }
