d1c7bd3bf22ad27d02efa5eac3d3b044da06de9d
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-19 11:54:57 -0500

    Add empty DbContext

---
+++ class-12/Demo/Demo12/Data/SchoolDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Demo12.Data
+{
+    // Inherit from EF Core DbContext
+    public class SchoolDbContext : DbContext
+    {
+        // Allow my context to be configured by magic
+        public SchoolDbContext(DbContextOptions options) : base(options) { }
+    }
+} 
