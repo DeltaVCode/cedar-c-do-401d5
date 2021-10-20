06f6e8f375e9db802969bc53acaab14c8e29744d
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-20 11:30:50 -0500

    Step 3: Take dependency on our scoped DbContext

--- class-13/Demo/Demo12/Services/Database/DatabaseTechnologyRepository.cs
+++ class-13/Demo/Demo12/Services/Database/DatabaseTechnologyRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Demo12.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace Demo12.Services.Database
 {
     public class DatabaseTechnologyRepository : ITechnologyRepository
     {
+        private readonly SchoolDbContext _context;
+
+        public DatabaseTechnologyRepository(SchoolDbContext context)
+        {
+            _context = context;
+        }
     }
 }
