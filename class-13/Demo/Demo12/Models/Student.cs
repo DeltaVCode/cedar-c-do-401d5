def7dde13de2799823816cec7d39e00bcd83d11a
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-20 09:43:44 -0500

    Add Students model

---
+++ class-13/Demo/Demo12/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo12.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+    }
+}
