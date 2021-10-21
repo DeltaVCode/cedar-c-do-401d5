bf488cb51e6157e520dca69707a5abc755018869
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-21 11:42:03 -0500

    Add Transcripts table

---
+++ class-14/Demo/Demo12/Migrations/20211021164141_AddTranscriptsTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Demo12.Migrations
+{
+    public partial class AddTranscriptsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
