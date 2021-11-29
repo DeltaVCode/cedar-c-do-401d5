5c7205877dea3251f12495051f47b00d8711b2cb
Keith Dahlby <keith@deltavcodeschool.com>
2021-11-29 11:31:11 -0600

    Add a Home class component

---
+++ class-36/demo/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HomeClassComponent extends React.Component {
+  render() {
+    return (
+      <h1>Hi from Class Component</h1>
+    )
+  }
+}
+
+export default HomeClassComponent;
