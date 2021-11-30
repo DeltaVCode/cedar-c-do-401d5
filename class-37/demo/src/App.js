23d5265813ab99ba7ae8c5dbf01a167bc1ccd2a6
Keith Dahlby <keith@deltavcodeschool.com>
2021-11-30 10:41:22 -0600

    Demo 27

---
+++ class-37/demo/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import People from './components/People'
+
+const data = [
+  { id: 12, name: 'Keith' },
+  { id: 15, name: 'Stacey' },
+  { id: 18, name: 'Craig' },
+  { id: 21, name: 'Andy' },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/people">People</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact>
+          <Home message="Welcome!" isAwesome />
+        </Route>
+        <Route path="/people">
+          <People people={data} title="Family" />
+        </Route>
+        <Route>
+          <h1>Not Found!</h1>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
