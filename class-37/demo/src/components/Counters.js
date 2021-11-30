396021da6ad40b6326160f581f4845e7fe55d148
Keith Dahlby <keith@deltavcodeschool.com>
2021-11-30 11:01:06 -0600

    Use useState for function component counter

--- class-37/demo/src/components/Counters.js
+++ class-37/demo/src/components/Counters.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Counters() {
   return (
     <>
       <h1>Counters!</h1>
+      <h2>Function Component</h2>
+      <CounterFunctionComponent />
+      <h2>Function Component 2</h2>
+      <CounterFunctionComponent />
       <h2>Class Component</h2>
       <CounterClassComponent />
     </>
@@ -11,6 +15,24 @@ function Counters() {
 }
 
 export default Counters;
+
+function CounterFunctionComponent() {
+  // destructure the array: first has state value, second has setter
+  const [counter, setCounter] = useState(0); // Initial value = 0
+
+  // Define handler functions inside the function
+  function handleClick() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <p>
+      Counter: {counter}
+      {' '}
+      <button onClick={handleClick}>+1</button>
+    </p>
+  )
+}
 
 class CounterClassComponent extends React.Component {
   constructor(props) {
