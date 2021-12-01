4a99a31c18b64c91d9b6e50f86e1ce26f9e6ab3f
Keith Dahlby <keith@deltavcodeschool.com>
2021-12-01 10:41:26 -0600

    Actually provide some fake context state

--- class-38/demo/src/hooks/useAuth.js
+++ class-38/demo/src/hooks/useAuth.js
@@ -1,7 +1,23 @@
+import { createContext, useContext } from "react"
+
+// React Magic!
+export const AuthContext = createContext();
 
 export default function useAuth() {
-  return {
+  const auth = useContext(AuthContext);
+  if (!auth) throw new Error("You forgot AuthProvider!");
+  return auth;
+}
+
+export function AuthProvider(props) {
+  const state = {
     user: null,
     // user: { username: 'Keith' },
-  }
+  };
+
+  return (
+    <AuthContext.Provider value={state}>
+      {props.children}
+    </AuthContext.Provider>
+  )
 }
--- class-38/demo/src/index.js
+++ class-38/demo/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './hooks/useAuth';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
