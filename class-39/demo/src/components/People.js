6ae0f67b5b9a323286defd2ffdd099349837dddc
Keith Dahlby <keith@deltavcodeschool.com>
2021-12-02 10:14:40 -0600

    Add some sort of count

--- class-39/demo/src/components/People.js
+++ class-39/demo/src/components/People.js
@@ -25,9 +25,12 @@ function People(props) {
   console.log('People props', props);
   const { people, title } = props;
 
+  console.log('Count five-letters')
+  const fiveLetterCount = people.filter(p => p.name.length === 5).length;
+
   return (
     <>
-      <h2>{title}</h2>
+      <h2>{title} ({fiveLetterCount})</h2>
       <ul>
         {people.map(person => (
           <li key={person.id}>{person.name}</li>
