761ec44c9582c563292cffc6ed1e31b1a37dd513
Keith Dahlby <keith@deltavcodeschool.com>
2021-12-02 11:14:22 -0600

    Do an authenticated delete request

--- class-39/demo/src/components/Todos.js
+++ class-39/demo/src/components/Todos.js
@@ -1,9 +1,28 @@
+import useAuth from "../hooks/useAuth";
 import useFetch from "../hooks/useFetch";
+import Auth from "./auth";
 
 const todoApi = 'https://deltav-todo.azurewebsites.net/api/v1/Todos';
 
 export default function Todos() {
   const { data, isLoading } = useFetch(todoApi);
+  const { user } = useAuth();
+
+  async function handleDelete(todo) {
+    console.log('Deleting...', todo);
+    if (!user) {
+      console.warn("Anonymous should not be allowed to delete!");
+      return;
+    }
+
+    // Ideally this would also be encapsulated in useFetch
+    await fetch(`${todoApi}/${todo.id}`, {
+      method: 'delete',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+  }
 
   if (isLoading) {
     return (<h2>Loading...</h2>)
@@ -12,7 +31,13 @@ export default function Todos() {
   return (
     <ul>
       {data.map(todo => (
-        <li key={todo.id}>{todo.title}</li>
+        <li key={todo.id}>
+          {todo.title}
+
+          <Auth permission='delete'>
+            <button onClick={() => handleDelete(todo)}>Delete</button>
+          </Auth>
+        </li>
       ))}
     </ul>
   )
